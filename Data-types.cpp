91d84c00d9dc70e98e4375fabddbfad4f254a0c4
Naman Gupta <67692721+naman153@users.noreply.github.com>
2020-09-09 10:37:16 +0530

    add

---
+++ Data-types.cpp
@@ -0,0 +1,23 @@
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    string s = "HackerRank ";
+    
+     string str {};
+    int a;
+    double b{0.0};
+    cin>>a;
+    cin>>b;
+    cin.ignore(); 
+    getline(cin,str);
+    cout<<a+i<<endl;
+    cout<<fixed<<setprecision(1)<<b+d<<endl;
+    cout<<s + str ;
+
+    return 0;
+}
